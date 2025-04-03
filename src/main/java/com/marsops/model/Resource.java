3832176b04a9203c52bf93c4571cfad27f4edd69
NikosLaspias <nikoslaspias@gmail.com>
2025-04-03 22:02:57 +0300

    Add custom constructor to Resource model for testing

--- src/main/java/com/marsops/model/Resource.java
+++ src/main/java/com/marsops/model/Resource.java
@@ -8,10 +8,17 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Resource {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
     private int quantity;
+
+    // Custom constructor για χρήση στα tests
+    public Resource(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
 }
