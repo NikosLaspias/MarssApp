df530a8aa2d53810c6d49a32b698c429611dd2ab
Nikos Laspias <148558723+NikosLaspias@users.noreply.github.com>
2025-04-03 21:24:44 +0300

    Create MarsopsCrudApplicationTest.java

---
+++ src/main/test/java/com/marsops/MarsopsCrudApplicationTest.java
@@ -0,0 +1,13 @@
+package com.marsops;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MarsopsCrudApplicationTest {
+
+    @Test
+    void dummyTest() {
+        // Απλά για να υπάρχει τουλάχιστον 1 test που περνάει
+        assertTrue(true);
+    }
+}
